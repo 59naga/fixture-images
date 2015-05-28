e271cf4aec71f67547cdbc9048a91f45dec81855
59naga <i59naga@icloud.com>
2015-05-28 16:58:01 +0900

    Add http/https props

--- all.js
+++ all.js
@@ -17,4 +17,40 @@ module.exports= {
     psd:  fs.readFileSync(__dirname+'/still.PSD'),
     tif:  fs.readFileSync(__dirname+'/still.TIF'),
   },
+  http: {
+    animated: {
+      gif:  'http://cdn.rawgit.com/59naga/fixture-images/master/animated.GIF',
+      png:  'http://cdn.rawgit.com/59naga/fixture-images/master/animated.PNG',
+      webp: 'http://cdn.rawgit.com/59naga/fixture-images/master/animated.WEBP',
+    },
+    still: {
+      gif:  'http://cdn.rawgit.com/59naga/fixture-images/master/still.GIF',
+      png:  'http://cdn.rawgit.com/59naga/fixture-images/master/still.PNG',
+      webp: 'http://cdn.rawgit.com/59naga/fixture-images/master/still.WEBP',
+
+      jpg:  'http://cdn.rawgit.com/59naga/fixture-images/master/still.JPG',
+      jxr:  'http://cdn.rawgit.com/59naga/fixture-images/master/still.JXR',
+      bmp:  'http://cdn.rawgit.com/59naga/fixture-images/master/still.BMP',
+      psd:  'http://cdn.rawgit.com/59naga/fixture-images/master/still.PSD',
+      tif:  'http://cdn.rawgit.com/59naga/fixture-images/master/still.TIF',
+    },
+  },
+  https: {
+    animated: {
+      gif:  'https://cdn.rawgit.com/59naga/fixture-images/master/animated.GIF',
+      png:  'https://cdn.rawgit.com/59naga/fixture-images/master/animated.PNG',
+      webp: 'https://cdn.rawgit.com/59naga/fixture-images/master/animated.WEBP',
+    },
+    still: {
+      gif:  'https://cdn.rawgit.com/59naga/fixture-images/master/still.GIF',
+      png:  'https://cdn.rawgit.com/59naga/fixture-images/master/still.PNG',
+      webp: 'https://cdn.rawgit.com/59naga/fixture-images/master/still.WEBP',
+
+      jpg:  'https://cdn.rawgit.com/59naga/fixture-images/master/still.JPG',
+      jxr:  'https://cdn.rawgit.com/59naga/fixture-images/master/still.JXR',
+      bmp:  'https://cdn.rawgit.com/59naga/fixture-images/master/still.BMP',
+      psd:  'https://cdn.rawgit.com/59naga/fixture-images/master/still.PSD',
+      tif:  'https://cdn.rawgit.com/59naga/fixture-images/master/still.TIF',
+    },
+  },
 };
